7230ea0d66c7e8c3b64c5ab6c7b9891a3e58592e
seunghyuen98 <mexim0905@gamil.com>
2023-07-11 16:46:10 +0900

    0711

---
+++ 0711/a_main.py
@@ -0,0 +1,9 @@
+import sys
+# sys.path.append('C:\Users\USER\Desktop\vscode Workspace\DX_DATA_Python\0711') # 현재 경로에서 모듈이나 패키지를 검색하도록 설정
+
+# print(sys.path)
+import other
+print(other.mypi)
+
+# import os
+# print(os.getcwd())
